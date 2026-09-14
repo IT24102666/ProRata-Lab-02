60da25e001236ecafc1d30ad40bbefe315700745
IT24102666 <it24102666@my.sliit.lk>
2026-09-14 16:42:54 +0530

    Add files via upload

---
+++ Q3/IT24102666Lab2Q3.java
@@ -0,0 +1,15 @@
+public class IT24102666Lab2Q3 {
+	public static void main(String[] args) {
+		double lengthSideA;
+		double lengthSideB;
+		double Hypotenuse;
+		
+		lengthSideA = 3;
+		lengthSideB = 4;
+		
+		Hypotenuse = Math.sqrt(lengthSideA*lengthSideA + lengthSideB*lengthSideB);
+
+		System.out.println("Length of Hypotenuse: " + Hypotenuse);
+
+		}
+}
