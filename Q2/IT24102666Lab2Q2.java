06d7e3c7789744b8e93c663dd160fa75325cfd13
IT24102666 <it24102666@my.sliit.lk>
2026-09-14 16:41:06 +0530

    Add files via upload

---
+++ Q2/IT24102666Lab2Q2.java
@@ -0,0 +1,14 @@
+public class IT24102666Lab2Q2 {
+	public static void main(String[] args) {
+		int lengthOfSquareSide;
+		double perimeterOfSquare;
+		double radiusOfCircle;
+		double perimeter;
+
+		lengthOfSquareSide = 10;
+		perimeterOfSquare = 4 * lengthOfSquareSide;
+		radiusOfCircle = perimeterOfSquare/(2*3.14);
+		System.out.println("Radius of the Circular Fence is: " + radiusOfCircle);
+
+		}
+}
